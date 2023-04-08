669c42dc247f64082d15294a81670c0139926bb6
Sigma2345 <mudit1978mars@gmail.com>
2023-04-08 18:25:29 +0530

    fixed bug check Block with piece

--- chess_app_1/lib/chess_board.dart
+++ chess_app_1/lib/chess_board.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:chess_app_1/game_coordinator.dart';
 import 'package:chess_app_1/pieces/chess_piece.dart';
 import "package:flutter/material.dart";
@@ -26,7 +28,7 @@ class _ChessBoardState extends State<ChessBoard> {
       (piece) => piece.name == "king" && piece.pieceColor == PlayerColor.white);
   late final ChessPiece blackKing = coordinator.pieces.firstWhere(
       (piece) => piece.name == "king" && piece.pieceColor == PlayerColor.black);
-
+  List<Location>? whiteKingCheckLocations = [], blackKingCheckLocations = [];
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Chess Board")),
@@ -66,42 +68,59 @@ class _ChessBoardState extends State<ChessBoard> {
             // removing captured piece
             pieces.remove(capturedPiece);
           }
+          whiteKingCheckLocations = isKingUnderCheck(whiteKing);
+          blackKingCheckLocations = isKingUnderCheck(blackKing);
           if (coordinator.currentTurn == PlayerColor.white) {
             coordinator.currentTurn = PlayerColor.black;
+            if (blackKingCheckLocations!.isNotEmpty)
+              print('Black King in Check');
           } else {
             coordinator.currentTurn = PlayerColor.white;
+            if (whiteKingCheckLocations!.isNotEmpty)
+              print('White King in Check');
           }
         });
       },
       onWillAccept: (piece) {
         if (piece == null) return false;
         if (coordinator.currentTurn != piece.pieceColor) return false;
-
-        List<Location>? whiteKingCheckLocations = isKingUnderCheck(whiteKing);
-        List<Location>? blackKingCheckLocations = isKingUnderCheck(blackKing);
-
         if (coordinator.currentTurn == PlayerColor.white &&
             whiteKingCheckLocations!.isNotEmpty) {
           //resolve double check
           if (piece != whiteKing) {
-            if (whiteKingCheckLocations.length > 1) {
+            if (whiteKingCheckLocations!.length > 1) {
               return false;
             }
-            Location checkLocation = whiteKingCheckLocations[0];
+            Location checkLocation = whiteKingCheckLocations![0];
+            bool checkBlock = false;
+            List<Location>? checkPath = pathBetweenKingAndPiece(whiteKing,
+                coordinator.pieceOfTile(checkLocation.x, checkLocation.y));
+            print(coordinator.pieceOfTile(checkLocation.x, checkLocation.y));
+            if (checkPath!.isNotEmpty) {
+              print('checking path');
+              for (int i = 0; i < checkPath.length && !checkBlock; i++) {
+                print('${checkPath[i].x}, ${checkPath[i].y}');
+                checkBlock = checkBlock ||
+                    piece.canMoveTo(
+                        checkPath[i].x, checkPath[i].y, coordinator.pieces);
+              }
+              if (checkBlock) return true;
+            }
             bool check = piece.canCapture(
                 checkLocation.x, checkLocation.y, coordinator.pieces);
             if (!check) {
               // piece cannot capture at that location
-              // print("piece cannot capture at that location");
+              print("piece cannot capture at that location");
               return false;
             }
+            // case where check path can be blocked
             return checkLocation == Location(x, y);
           } else {
             //king moves
             // -> move => pieces of other color are not attacking the square
             // -> capture => pieces of other color not on that square not protecting
             return (whiteKing.canMoveTo(x, y, coordinator.pieces) ||
-                    whiteKing.canMoveTo(x, y, coordinator.pieces)) &&
+                    whiteKing.canCapture(x, y, coordinator.pieces)) &&
                 canKingCaptureOrMove(x, y, whiteKing);
           }
           // add how the king moves in check and doubleCheck
@@ -109,30 +128,45 @@ class _ChessBoardState extends State<ChessBoard> {
             blackKingCheckLocations!.isNotEmpty) {
           //resolve double check
           if (piece != blackKing) {
-            if (blackKingCheckLocations.length > 1) {
+            if (blackKingCheckLocations!.length > 1) {
               return false;
             }
-            Location checkLocation = blackKingCheckLocations[0];
+            Location checkLocation = blackKingCheckLocations![0];
+            bool checkBlock = false;
+            List<Location>? checkPath = pathBetweenKingAndPiece(blackKing,
+                coordinator.pieceOfTile(checkLocation.x, checkLocation.y));
+            print(coordinator.pieceOfTile(checkLocation.x, checkLocation.y));
+            if (checkPath!.isNotEmpty) {
+              print('checking path');
+              for (int i = 0; i < checkPath.length && !checkBlock; i++) {
+                print('${checkPath[i].x}, ${checkPath[i].y}');
+                checkBlock = checkBlock ||
+                    piece.canMoveTo(
+                        checkPath[i].x, checkPath[i].y, coordinator.pieces);
+              }
+              if (checkBlock) return true;
+            }
             bool check = piece.canCapture(
                 checkLocation.x, checkLocation.y, coordinator.pieces);
             if (!check) {
               // piece cannot capture at that location
-              // print("piece cannot capture at that location");
+              print("piece cannot capture at that location");
               return false;
             }
+            // case where check path can be blocked
             return checkLocation == Location(x, y);
           } else {
             //king moves
             // -> move => pieces of other color are not attacking the square
             // -> capture => pieces of other color not on that square not protecting
             return (blackKing.canMoveTo(x, y, coordinator.pieces) ||
-                    blackKing.canMoveTo(x, y, coordinator.pieces)) &&
+                    blackKing.canCapture(x, y, coordinator.pieces)) &&
                 canKingCaptureOrMove(x, y, blackKing);
           }
           // add how the king moves in check and doubleCheck
         }
 
-        if(piece==whiteKing) {
+        if (piece == whiteKing) {
           //king moves
           // -> move => pieces of other color are not attacking the square
           // -> capture => pieces of other color not on that square not protecting
@@ -153,8 +187,9 @@ class _ChessBoardState extends State<ChessBoard> {
         bool canMoveTo = piece.canMoveTo(x, y, pieces);
         bool canCapture = piece.canCapture(x, y, pieces);
 
-        // bool ans = canMoveTo || canCapture;
-        // print("${ans ? "can move " : "cannot move"} ${piece.pieceColor.toString()} ${piece.name} to ($x, $y)");
+        bool ans = canMoveTo || canCapture;
+        print(
+            "${ans ? "can move " : "cannot move"} ${piece.pieceColor.toString()} ${piece.name} to ($x, $y)");
         return canMoveTo || canCapture;
       },
       builder: (context, data, rejects) => InkWell(
@@ -209,6 +244,57 @@ class _ChessBoardState extends State<ChessBoard> {
       locationsOfCheckingPieces.add(piecesCheckingKing[i].location);
     }
     return locationsOfCheckingPieces;
+  }
+
+  List<Location>? pathBetweenKingAndPiece(
+      ChessPiece king, ChessPiece? attackingPiece) {
+    List<Location>? ans = [];
+    if (attackingPiece!.name == "knight") {
+      return ans;
+    }
+
+    int dx = attackingPiece.location.x - king.location.x;
+    int dy = attackingPiece.location.y - king.location.y;
+
+    if (dx == 0 || dy == 0) {
+      if (dy == 0) {
+        int stRow = min(attackingPiece.location.x, king.location.x) + 1;
+        int enRow = max(attackingPiece.location.x, king.location.x);
+        List<Location>? blockableSquares = [];
+        while (stRow < enRow) {
+          blockableSquares.add(Location(stRow, king.location.y));
+          stRow++;
+        }
+        return ans;
+      } else {
+        int stCol = min(attackingPiece.location.y, king.location.y) + 1;
+        int enCol = max(attackingPiece.location.y, king.location.y);
+        List<Location>? blockableSquares = [];
+        while (stCol < enCol) {
+          blockableSquares.add(Location(king.location.x, stCol));
+          stCol++;
+        }
+        return ans;
+      }
+    }
+    List<Location>? blockableSquares = [];
+    int st_row = attackingPiece.location.x;
+    int st_col = attackingPiece.location.y;
+    while (st_row != king.location.x) {
+      if (st_row < king.location.x) {
+        st_row++;
+      } else {
+        st_row--;
+      }
+      if (st_col < king.location.y) {
+        st_col++;
+      } else {
+        st_col--; 
+      }
+      if(st_row != king.location.x)blockableSquares.add(Location(st_row, st_col));
+    }
+    return blockableSquares;
+    // return ans;
   }
 
   bool canKingCaptureOrMove(
